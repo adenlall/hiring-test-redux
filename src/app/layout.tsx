fc52b1ba6d88152d54cfd1c3bcb1cd1e112c5f96
adenlall <adenlall@skiff.com>
2024-08-02 12:25:00 +0100

    fixe SSR building

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "Janah Bilal - Hiring Test",
   description:
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+export const dynamic = "force-dynamic";
 import { deleteIndexedDB } from "@/db/wipeup"
 import toast from "react-hot-toast";
 
